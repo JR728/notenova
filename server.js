7d1007becb2f3f3a014ea9285bedcc3a36624bfa
JR728 <135184435+JR728@users.noreply.github.com>
2023-09-06 22:24:29 -0500

    added delete path

--- server.js
+++ server.js
@@ -40,6 +40,24 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    const noteIdToDelete = parseInt(req.params.id);
+  
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+  
+      const notes = JSON.parse(data);
+      const updatedNotes = notes.filter(note => note.id !== noteIdToDelete);
+  
+      fs.writeFile('./db/db.json', JSON.stringify(updatedNotes), 'utf8', err => {
+        if (err) throw err;
+  
+        res.json({ message: `Note with ID ${noteIdToDelete} deleted successfully` });
+      });
+    });
+});
+  
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
